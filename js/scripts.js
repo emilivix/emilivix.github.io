2317e0896d448add9c8ea748edc21cef02db0f03
Emil <emilivixviii@gmail.com>
2024-07-10 02:32:44 +0200

    update files

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,13 @@ function toggleMenu() {
         sidebar.style.display = 'block';
     }
 }
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
